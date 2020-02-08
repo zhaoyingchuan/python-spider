b84cb2751bb28c7576b9ee34a6e2f6fce6414d34
zhaoyingchuan <18146612665@163.com>
2020-02-08 19:56:15 +0800

    修改代码

--- 3.获取维基文库页面的文本.py
+++ 3.获取维基文库页面的文本.py
@@ -1,22 +1,24 @@
 #coding=utf-8
-import re
+
 import requests
 from lxml import etree
-
 
 headers = {
 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Ap\
 pleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Sa\
 fari/537.36"
 }
-url = r"https://zh.wikisource.org/zh-hans/脂硯齋重評石頭記/第二十二回"
+url = r"https://zh.wikisource.org/wiki/脂硯齋重評石頭記/第三回"
 
 response=requests.get(url,headers=headers).text
 
 html=etree.HTML(response)
 
-result=html.xpath("//div/p/text()") #获取所有span标签的信息
-# print(result)
+# title1=html.xpath("//div/table/tbody/tr/td/text()")
+# print(title1[2])
+
+result=html.xpath("//div/p/text()")
+
 t = ''
 for i in result:
     t=t+i
